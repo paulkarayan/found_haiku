642354cc892cd601b521123fdbe56aee35926df6
paulkarayan <paulkarayan@gmail.com>
2013-01-21 23:15:14 -0800

    all thats left to do: arrange as 5/7/5
    well - its very slow, still has debug messages.
    may want to parallelize :) this is crying out for map reduce

--- found_haiku.py
+++ found_haiku.py
@@ -15,10 +15,10 @@ Example:
 python found_haiku.py http://www.paulkarayan.com
 
 """
-# todo:
+
 # 1) retrive text from URL
 # 2) parse into sentences / sentence fragments, then into words
-# 3) count the syllables in each word & sentence
+# 3) count the syllables in each word & sentence. save 5 or 7 syllable ones.
 # 4) put sentences that have 5 or 7 syllables together
 # 5) print out haiku
 # 6) ?
@@ -28,80 +28,132 @@ python found_haiku.py http://www.paulkarayan.com
 worddict = {}
 
 
-def main(sentence):
-	#just a dummy so i can run from command line
-	print(parsesentences(sentence))
 
-def parsesentences(sentence):
-# through the glory of regex, break apart the sentence
-# see http://stackoverflow.com/questions/367155/splitting-a-string-into-words-and-punctuation
-	
-	wordlist = re.findall(r"[\w']+|[.,!?;]", "%s" % sentence)
-	syllablecounter = 0
 
-	for word in wordlist:
-		print(word)
-		syllablecounter += countsyllables(word)
 
-		print(syllablecounter)
-
-	if syllablecounter == 5:
-		worddict[sentence] = syllablecounter
-	elif syllablecounter == 7:
-		worddict[sentence] = syllablecounter
-	else:
-		print(syllablecounter, ' is not a good match!')
-
-	return(worddict)
 
 def countsyllables(word):
 
-	name="text"
-	name="optionSyllableCount"
-	name="optionWordCount"
+  name="text"
+  name="optionSyllableCount"
+  name="optionWordCount"
 
 
-	url = 'http://www.wordcalc.com/index.php'
-	post_data = urllib.urlencode(
-    	{'text': '%s' % word}) 
-	post_data = '%s&optionSyllableCount&optionWordCount' % post_data
+  url = 'http://www.wordcalc.com/index.php'
+  post_data = urllib.urlencode(
+        {'text': '%s' % word}) 
+  post_data = '%s&optionSyllableCount&optionWordCount' % post_data
 
-	cnxn = urllib.urlopen(url, post_data)
-	response = cnxn.read()
-	cnxn.close()
+  cnxn = urllib.urlopen(url, post_data)
+  response = cnxn.read()
+  cnxn.close()
 
-	soup = BeautifulSoup(response)
-	h3_matches = [h3 for h3 in soup.findAll('h3') if h3.text == 'Statistics']
-	if len(h3_matches) != 1:
-  		raise Exception('Wrong number of <h3>Statistics</h3>')
-	h3_match = h3_matches[0]
-	table = h3_match.findNextSibling('table')
+  soup = BeautifulSoup(response)
+  h3_matches = [h3 for h3 in soup.findAll('h3') if h3.text == 'Statistics']
+  if len(h3_matches) != 1:
+    raise Exception('Wrong number of <h3>Statistics</h3>')
+  h3_match = h3_matches[0]
+  table = h3_match.findNextSibling('table')
 
-	td_matches = [td for td in table.findAll('td')
-              	if td.text == 'Syllable Count']
-	if len(td_matches) != 1:
-  		raise Exception('Wrong number of <td>Syllable Count</td>')
-	td_match = td_matches[0]
+  td_matches = [td for td in table.findAll('td')
+                if td.text == 'Syllable Count']
+  if len(td_matches) != 1:
+    raise Exception('Wrong number of <td>Syllable Count</td>')
+  td_match = td_matches[0]
 
-	td_value = td_match.findNextSibling('td')
-	syllable_count = int(td_value.text)
+  td_value = td_match.findNextSibling('td')
+  syllable_count = int(td_value.text)
 
-	return(syllable_count)
+  return(syllable_count)
+
+def parsesentences(wordlist):
+# through the glory of regex, break apart the sentence
+# see http://stackoverflow.com/questions/367155/splitting-a-string-into-words-and-punctuation
+
+#to do: need to count for each sentance! despite what this says, i think
+# that wordlist 
+
+  for sentence in wordlist:
+    #sentence is the unicode string, unbroken  
+    print(sentence, '<-sentence variable')
+    words = re.findall(r"[\w']+|[.,!?;]", "%s" % str(sentence))
+    #words is the list of unicode strings in a sentence 
+    print(words)
+    syllablecounter = 0
+    sensyllablecounter = 0
+    for word in words:
+      #print(str(word))
+      syllablecounter = countsyllables(str(word))
+      sensyllablecounter += syllablecounter
+      print(sensyllablecounter)
+
+
+    if sensyllablecounter == 5:
+      worddict[str(sentence)] = sensyllablecounter
+    elif sensyllablecounter == 7:
+      worddict[str(sentence)] = sensyllablecounter
+    else:
+      print('%s is not a good match!' % sensyllablecounter)
+
+  return(worddict)
+
+
+def visible(element):
+  if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+    return False
+  elif re.match('<!--.*-->', str(element)):
+    return False
+  return True
+
+  
+
+
+def removeblanks(element):
+  if element == '\n':
+    return False
+  else:
+    return True
+
+
+
+def pulldownurltext(url):
+
+  #retrieve visible text from an url based on:
+  #http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text
+
+  #test for now:
+  #url = 'http://www.paulkarayan.com'
+
+  html = urllib.urlopen(url).read()
+  soup = BeautifulSoup(html)
+  texts = soup.findAll(text=True)
+  visible_texts = filter(visible, texts)
+  wordlist = filter(removeblanks, visible_texts)       
+  # return a list of visible text with u'content', and new lines removed
+  return(wordlist)
+
+
+def main(url):
+  
+  returnedwl = pulldownurltext(url)
+  print('I\'ve got a word list!')
+  print(parsesentences(returnedwl))
+
 
 
 if __name__ == '__main__':
     try:
         
         # eventually:  
-        #url = sys.argv[1]
+        url = sys.argv[1]
         
         #but for now:
-        sentence = sys.argv[1]
+        #sentence = sys.argv[1]
        
 
     except:
         print usage
         sys.exit(-1)
 
-#this will change to url
-    main(sentence)
+
+    main(url)
